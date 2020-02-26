7de8275401ef00b1bd3cc40351cc795786af72e9
AherreDev <aherredev@gmail.com>
2020-02-26 17:26:52 -0600

    [Add] Completed Cantidad

--- models/Cantidad.model.js
+++ models/Cantidad.model.js
@@ -1,11 +1,16 @@
 class Cantidad {
   valor;
   unidad;
+  /**
+   *
+   * @param {number} valor
+   * @param {string} unidad
+   */
   constructor(valor, unidad) {
     this.valor = valor;
     this.unidad = unidad;
   }
-  getDescripcion = () => {};
+  getDescripcion = () => `${this.valor} ${this.unidad}`;
 }
 
 export default Cantidad;
